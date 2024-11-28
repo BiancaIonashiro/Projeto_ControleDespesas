f78e4cab60e31da0055b9b847ae62d7d898ee37b
Bianca <bianca.ionashiro@gmail.com>
2024-11-28 09:47:54 -0300

    Ajuste  configuração.

--- Projeto_ControleDespesas/Model/Entity/ControleDespesa.cs
+++ Projeto_ControleDespesas/Model/Entity/ControleDespesa.cs
@@ -2,15 +2,13 @@
 
 namespace Projeto_ControleDespesas.Model.Entity;
 
-public class ControleDespesa// Planta com o Projeto Modelo com a ideia de reutilização quando necessário;
+public class ControleDespesa
 {
     public int Id { get; set; }
-    public string? Descricao { get; set; } // Atributo = Caracteristica;
-    //Conjunto de Atributos(Características do objeto material ou imaterial);
-    public Categoria Categoria { get; set; } // Encapsulamento(Modificador de Acesso/Permissão): P/ este ser "Encontrado/Visivel" ao ser
-                                             // "Instanciado(Acessado/Chamado)" por outras classes;
-    public SubCategoria SubCategoria { get; set; } // Tipo de dado do atributo;
-    public DateTime DataCompra {  get; set; } // { Consulta; Adiciona Valor}; 
+    public string? Descricao { get; set; } 
+    public Categoria Categoria { get; set; } 
+    public SubCategoria SubCategoria { get; set; }
+    public DateTime DataCompra {  get; set; } 
     public decimal Quantidade { get; set;}
     public decimal Preco { get; set;}
     public decimal OutrosCustos { get; set;}
--- Projeto_ControleDespesas/Program.cs
+++ Projeto_ControleDespesas/Program.cs
@@ -12,33 +12,16 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-//
+
+
 builder.Services.AddEntityFrameworkSqlServer()
     .AddDbContext<SistemaControleDbContex>(
        options => options.UseSqlServer(builder.Configuration.GetConnectionString("DataBase"))
        );
 
 builder.Services.AddTransient<IControleDespesaRepository, ControleDespesaRepository>();
-//
 
-/* Criação do objeto: Declaro Tipo(Classe), crio variavel("Caixa" p/ armazenar valores) = Novo (Instancia/Chamado) da minha Classe(), através do 
- * método, exemplo: 
- * Tipo(Classe) ->ControleDespesasModel<- Var(Objeto dessa classe) ->controleDespesasModel(minusculo)<- = 
- * (Usado p/ instancia) ->new<- Metodo() ->ControleDespesasModel();<- , ou seja, criação de uma instância do ControleDespesasModel: */
 
-//ControleDespesasModel controleDespesasModel = new ControleDespesasModel(); 
-
-//objeto.Propriedade = 123; <-Atributo **Geralmente "setar" valor em métodos é o mais indicado e performatico, ou seja, pode-se
-//inserir {get; private set;} p/ só consultar e não alterar ou private double Saldo {get;set;} indicado p/ proteger totalmente o atributo p/
-//somente a Classe poder manipular o valor;
-
-//objeto.Propriedade(caracteristica unica da minha classe que pode ser Quantidade, Preco, ..., etc)->(Valor desejado p/ add); <-Metodo **Geralmente
-//o mais utilizado, pois qualquer validação é utilizado dentro do metodo que é criado p/ executar, sendo facil manutenção ao add valores"setar";
-
-//controleDespesasModel.Limite = 123;
-//controleDespesasModel.Deposito(1500);
-
-//double saldoTotal = conta.Saldo();
 
 var app = builder.Build();
 
